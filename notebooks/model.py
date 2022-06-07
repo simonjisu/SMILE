cd55d9a964a3f5582bf4d44ac6427b71ef9fc8fa
simonjisu <simonjisu@gmail.com>
2022-06-08 00:30:51 +0900

    update modules

---
+++ notebooks/model.py
@@ -0,0 +1,238 @@
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+
+class LSTM(nn.Module):
+    def __init__(self, input_size: int, hidden_size: int, num_layers: int):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, bidirectional=False)
+        self.lnorm = nn.LayerNorm(hidden_size)
+    
+    def forward(self, x: torch.tensor):
+        # x: (B, T, I)
+        o, (h, _) = self.lstm(x) # o: (B, T, H) / h: (1, B, H)
+        normed_context = self.lnorm(h)
+        return normed_context
+
+class LSTMAttention(nn.Module):
+    def __init__(self, input_size: int, hidden_size: int, num_layers: int):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, bidirectional=False)
+        self.lnorm = nn.LayerNorm(hidden_size)
+
+    def forward(self, x: torch.tensor, rt_attn: bool=False):
+        # x: (B, T, I)
+        o, (h, _) = self.lstm(x) # o: (B, T, H) / h: (1, B, H)
+        score = torch.bmm(o, h.permute(1, 2, 0)) # (B, T, H) x (B, H, 1)
+        attn = torch.softmax(score, 1).squeeze(-1)  # (B, T)
+        context = torch.bmm(attn.unsqueeze(1), o).squeeze(1)  # (B, 1, T) x (B, T, H)
+        normed_context = self.lnorm(context)  # (B, H)
+        if rt_attn:
+            return normed_context, attn
+        else:
+            return normed_context, None
+
+class MappingNet(nn.Module):
+    def __init__(self, hidden_size):
+        super().__init__()
+        self.rn = nn.Sequential(
+            nn.Linear(hidden_size, 2*hidden_size, bias=False),
+            nn.ReLU(),
+            nn.Linear(2*hidden_size, 2*hidden_size, bias=False),
+        )
+
+    def forward(self, x: torch.tensor):
+        # x: (B, H)
+        outputs = self.rn(x)
+        return outputs
+
+class Model(nn.Module):
+    def __init__(
+            self, 
+            feature_size: int, 
+            hidden_size: int, 
+            output_size: int, 
+            num_layers: int, 
+            drop_rate: float, 
+            n_sample: int,
+            inner_lr_init: float,
+            finetuning_lr_init: float
+        ):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.parameter_size = hidden_size*output_size
+        self.n_sample = n_sample
+        
+        self.inner_lr = nn.Parameter(torch.FloatTensor([inner_lr_init]))
+        self.finetuning_lr = nn.Parameter(torch.FloatTensor([finetuning_lr_init]))
+
+        # Network
+        self.dropout = nn.Dropout(drop_rate)
+        self.feature_transform = nn.Linear(feature_size, hidden_size)
+        self.lstm = LSTMAttention(hidden_size, hidden_size, num_layers)  # encode
+        self.mapping_net = MappingNet(hidden_size)  # to generate z(latent)
+        self.decoder = nn.Linear(hidden_size, 2*self.parameter_size, bias=False)
+        self.prob_layer = nn.LogSoftmax(dim=1) if output_size >=2 else nn.LogSigmoid()
+
+        # Loss
+        self.loss_fn = nn.NLLLoss()
+
+    def encode(self, inputs, rt_attn: bool=False):
+        # inputs: (B, T, I)
+        inputs = self.dropout(inputs)
+        inputs = self.feature_transform(inputs)
+        encoded, attn = self.lstm(inputs, rt_attn)  # B, H
+        return encoded, attn
+
+    def get_z(self, inputs, rt_attn: bool=False):
+        # inputs: (B, T, I)
+        encoded, attn = self.encode(inputs, rt_attn=rt_attn)
+        hs = self.mapping_net(encoded)
+
+        z, dist = self.sample(hs, size=self.hidden_size)
+        return encoded, z, dist, attn
+
+    def sample(self, distribution_params, size):
+        mean, log_std = distribution_params[:, :size], distribution_params[:, size:]
+        std = torch.exp(log_std)
+        dist = torch.distributions.Normal(torch.zeros_like(mean), torch.ones_like(std))
+        z = dist.rsample()
+        return mean + std*z, dist
+
+    def decode(self, z):
+        param_hs = self.decoder(z)
+        parameters, _ = self.sample(param_hs, size=self.parameter_size)
+        return parameters
+
+    def predict(self, encoded, parameters):
+        theta = parameters.view(-1, self.hidden_size, self.output_size)
+        scores = encoded.unsqueeze(1).bmm(theta).squeeze()
+        probs = self.prob_layer(scores)
+        return probs
+        
+    def cal_accuracy(self, log_probs, target):
+        if self.output_size >= 2:
+            pred = log_probs.argmax(1)
+        else:
+            pred = (torch.exp(log_probs) >= 0.5).long()
+        correct = pred.eq(target).sum()
+        acc = correct / len(target)
+        return acc
+    
+    def reset_records(self):
+        self.records = {}
+
+    def inner_loop(self, data, n_inner_step: int=5, rt_attn: bool=False):
+        support_X, support_y = data['support'], data['support_labels']
+        support_encoded, support_z, support_dist, support_attn = self.get_z(support_X, rt_attn=rt_attn)
+        kld_loss = self.cal_kl_div(support_dist, support_z)
+        
+        z_init = support_z.clone().detach()
+
+        # inner adaptation to z
+        for i in range(n_inner_step):
+            support_z.retain_grad()
+            parameters = self.decode(support_z)
+            support_log_probs = self.predict(support_encoded, parameters)
+            train_loss = self.loss_fn(support_log_probs, support_y)
+            train_loss.backward(retain_graph=True)
+
+            support_z = support_z - self.inner_lr * support_z.grad.data
+
+        z_penalty = torch.mean((z_init - support_z)**2)
+        return support_encoded, support_z, kld_loss, z_penalty, support_attn
+
+    def outer_loop(self, data, support_z, support_encoded, n_finetuning_step: int=5, rt_attn: bool=False):
+        # finetuning inner + validation
+        
+        # inner loop prediction
+        support_y, query_X, query_y = data['support_labels'], data['query'], data['query_labels']
+        parameters = self.decode(support_z)
+        parameters.retain_grad()
+        support_log_probs = self.predict(support_encoded, parameters)
+        train_loss = self.loss_fn(support_log_probs, support_y)
+        train_acc = self.cal_accuracy(support_log_probs, support_y)
+
+        # logging
+        self.records['Support Loss'] = train_loss.item()
+        self.records['Support Accuracy'] = train_acc.item()
+        self.records['Inner LR'] = float(self.inner_lr)
+        self.records['Finetuning LR'] = float(self.finetuning_lr)
+        self.records['Latents'] = support_z.clone().detach().cpu().numpy()
+
+        # finetuning adaptation to parameters
+        for i in range(n_finetuning_step):
+            train_loss.backward(retain_graph=True)
+            parameters = parameters - self.finetuning_lr * parameters.grad
+            parameters.retain_grad()
+            support_log_probs = self.predict(support_encoded, parameters)
+            train_loss = self.loss_fn(support_log_probs, support_y)
+
+
+        # meta validation     
+        query_encoded, *_, query_attn = self.encode(query_X, rt_attn=rt_attn)
+        query_log_probs = self.predict(query_encoded, parameters)
+        query_loss = self.loss_fn(query_log_probs, query_y)
+        query_acc = self.cal_accuracy(query_log_probs, query_y)
+
+        return query_loss, query_acc, query_attn
+
+    def cal_kl_div(self, dist, z):
+        normal = torch.distributions.Normal(torch.zeros_like(z), torch.ones_like(z))
+        return torch.mean(dist.log_prob(z) - normal.log_prob(z))
+
+    def cal_total_loss(self, query_loss, kld_loss, z_penalty, beta, gamma, lambda2):
+        orthogonality_penalty = self.orthgonality_constraint(list(self.decoder.parameters())[0])
+        total_loss = query_loss + beta*kld_loss + gamma*z_penalty + lambda2*orthogonality_penalty
+        # loggings
+        self.records['KLD Loss'] = kld_loss.item()
+        self.records['Z Penalty'] = z_penalty.item()
+        self.records['Orthogonality Penalty'] = orthogonality_penalty.item()
+        
+        return total_loss
+
+    def orthgonality_constraint(self, params):
+        # purpose: encourages the dimensions of the latend code as well as the decoder network to be maximally expressive
+        # number of class x hidden_size x 2(mean, std)
+        p_dot = params.mm(params.transpose(0, 1))
+        p_norm = torch.norm(params, dim=1, keepdim=True) + 1e-15
+        corr = p_dot / p_norm.mm(p_norm.transpose(0, 1))
+        corr.masked_fill_(corr>1.0, 1.0)
+        corr.masked_fill_(corr<-1.0, -1.0)
+        I = torch.eye(corr.size(0)).to(corr.device)
+        orthogonality_penalty = torch.mean((corr - I)**2)
+        return orthogonality_penalty
+
+    def forward(
+            self, data, 
+            n_inner_step: int=5, 
+            n_finetuning_step:int =5, 
+            rt_attn: bool=False
+        ):
+        self.reset_records()
+        support_encoded, support_z, kld_loss, z_penalty, support_attn = self.inner_loop(data, n_inner_step, rt_attn)
+        query_loss, query_acc, query_attn = self.outer_loop(data, support_z, support_encoded, n_finetuning_step, rt_attn)
+        return query_loss, query_acc, kld_loss, z_penalty, support_attn, query_attn
+
+    def meta_run(self, data, 
+            beta: float=0.001, 
+            gamma: float=1e-9, 
+            lambda2: float=0.1,
+            n_inner_step: int=5, 
+            n_finetuning_step:int =5,
+            rt_attn: bool=False
+        ):
+        query_loss, query_acc, kld_loss, z_penalty, support_attn, query_attn = self(
+            data, n_inner_step, n_finetuning_step, rt_attn
+        )
+        total_loss = self.cal_total_loss(query_loss, kld_loss, z_penalty, beta, gamma, lambda2)
+        # logging
+        self.records['Query Loss'] = total_loss.item()
+        self.records['Query Accuracy'] = query_acc.item()
+        if query_attn is not None:
+            self.records['Query Attn'] = query_attn.detach().cpu().numpy()
+        if support_attn is not None:
+            self.records['Support Attn'] = support_attn.detach().cpu().numpy()
+
+        return total_loss, self.records
---
+++ notebooks/trainer.py
@@ -0,0 +1,171 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from pathlib import Path 
+from torch.utils.tensorboard import SummaryWriter
+
+class Trainer():
+    def __init__(
+            self, 
+            log_dir, 
+            total_steps,
+            n_inner_step, 
+            n_finetuning_step, 
+            n_valid_step,
+            every_valid_step,
+            beta,
+            gamma,
+            lambda1,
+            lambda2,
+            outer_lr,
+            clip_value,
+            device: str='cpu',
+            print_step: int=5
+        ):
+        self.device = device
+        self.print_step = print_step
+        self.total_steps = total_steps
+        self.n_inner_step = n_inner_step
+        self.n_finetuning_step = n_finetuning_step
+        self.n_valid_step = n_valid_step
+        self.every_valid_step = every_valid_step
+        
+        self.beta = beta
+        self.gamma = gamma
+        self.lambda1 = lambda1  # penalty on model(encoder, mapping_net, decoder) parameters
+        self.lambda2 = lambda2  # penalty on decoder
+        self.outer_lr = outer_lr
+        self.clip_value = clip_value
+        
+
+        # check if exp exists
+        self.log_dir = Path(log_dir)
+        exp_dirs = list(self.log_dir.glob('exp'))
+        exp_num = exp_dirs[-1].name[3:] if exp_dirs else 1
+        self.exp_dir = self.log_dir / f'exp{exp_num}'
+        self.writer = SummaryWriter(str(self.exp_dir))
+        self.ckpt_path = self.exp_dir / 'checkpoints'
+        if not self.ckpt_path.exists():
+            self.ckpt_path.mkdir()
+        self.log_keys = [
+            'Support Loss', 'Support Accuracy', 'Query Loss', 'Query Accuracy', 
+            'Inner LR', 'Finetuning LR', 'KLD Loss', 'Z Penalty', 'Orthogonality Penalty'
+        ]
+
+    def map_to_tensor(self, tasks, device: None | str=None):
+        if device is None:
+            device = torch.device('cpu')
+        else:
+            device = torch.device(device)
+        tensor_tasks = {}
+        for k, v in tasks.items():
+            tensor = torch.LongTensor if 'labels' in k else torch.FloatTensor
+            tensor_tasks[k] = tensor(np.array(v)).to(device)
+        return tensor_tasks
+
+    def step_batch(self, model, batch_data):
+        total_loss, records = model.meta_run(
+            data=batch_data, 
+            beta=self.beta, 
+            gamma=self.gamma, 
+            lambda2=self.lambda2, 
+            n_inner_step=self.n_inner_step, 
+            n_finetuning_step=self.n_finetuning_step, 
+            rt_attn=False
+        )
+        return total_loss, records
+
+    def manual_model_eval(self, model, mode=False):
+        # [PyTorch Issue] RuntimeError: cudnn RNN backward can only be called in training mode
+        # cannot use model.eval()
+        # https://stackoverflow.com/questions/51433378/what-does-model-train-do-in-pytorch
+        for module in model.children():
+            # model.training = mode
+            if isinstance(module, nn.Dropout):
+                module.train(mode)
+        return model
+
+    def meta_train(self, model, meta_trainset):
+        model = model.to(self.device)
+        lr_list = ['inner_lr', 'finetuning_lr']
+        params = [x[1] for x in list(filter(lambda k: k[0] not in lr_list, model.named_parameters()))]
+        lr_params = [x[1] for x in list(filter(lambda k: k[0] in lr_list, model.named_parameters()))]
+        optim = torch.optim.Adam(params, lr=self.outer_lr, weight_decay=self.lambda1)
+        optim_lr = torch.optim.Adam(lr_params, lr=self.outer_lr)
+        best_eval_acc = 0.0
+
+        for step in range(self.total_steps):
+            # Meta Train
+            model.train()
+            optim.zero_grad()
+            optim_lr.zero_grad()
+            train_tasks = meta_trainset.generate_tasks()
+            train_records = {k: [] for k in self.log_keys}
+            for window_size, tasks in train_tasks.items():
+                batch_data = self.map_to_tensor(tasks, device=self.device)
+                total_loss, records = self.step_batch(model, batch_data)
+                total_loss.backward()
+                nn.utils.clip_grad_value_(model.parameters(), self.clip_value)
+                nn.utils.clip_grad_norm_(model.parameters(), self.clip_value)
+                optim.step()
+                optim_lr.step()
+                for key, v in records.items():
+                    if (key in ['Latents']):
+                        self.writer.add_histogram(f'Latents-WinSize={window_size}', records['Latents'], step)
+                    else:
+                        train_records[key].append(v)
+                        self.writer.add_scalar(f'Train-WinSize={window_size}-{key}', v, step)
+                
+            # logging summary(average score for all window size tasks)
+            for key in self.log_keys:
+                self.writer.add_scalar(f'Train-{key}', np.mean(train_records[key]), step)
+                
+            if (step % self.print_step == 0) or (step == self.total_steps-1):
+                print(f'[Meta Train]({step+1}/{self.total_steps})')
+                for i, key in enumerate(self.log_keys):
+                    s1 = '  ' if (i == 0) or (i == 4) else ''
+                    s2 = '\n' if i == 3 else " | "
+                    print(f'{s1}{key}: {np.mean(train_records[key]):.4f}', end=s2)
+                print()
+                
+            # Meta Valid
+            if (step % self.every_valid_step == 1) or (step == self.total_steps-1):
+                # [PyTorch Issue] RuntimeError: cudnn RNN backward can only be called in training mode
+                # cannot use model.eval()
+                model = self.manual_model_eval(model, mode=False)
+
+                valid_records = {'Accuracy': [], 'Loss': []}
+                # n_valid_step x window_size 
+                for val_step in range(self.n_valid_step):
+                    valid_step_loss = []
+                    valid_step_acc = []
+                    valid_tasks = meta_trainset.generate_tasks()
+                    for window_size, tasks in valid_tasks.items():
+                        batch_data = self.map_to_tensor(tasks, device=self.device)
+                        _, records = self.step_batch(model, batch_data)
+                        valid_step_loss.append(records['Query Loss'])
+                        valid_step_acc.append(records['Query Accuracy'])
+
+                    valid_records['Accuracy'].append(valid_step_acc)
+                    valid_records['Loss'].append(valid_step_acc)
+                # average window loss and accruacy: mean by n_valid_step
+                valid_records['Accuracy'] = np.mean(valid_records['Accuracy'], axis=0)
+                valid_records['Loss'] = np.mean(valid_records['Loss'], axis=0)
+
+                for key in ['Accuracy', 'Loss']:
+                    for i, window_size in enumerate(meta_trainset.window_sizes):
+                        self.writer.add_scalar(f'Valid-WinSize={window_size}-{key}', valid_records[key][i], step)
+                    self.writer.add_scalar(f'Valid-Task {key}', np.mean(valid_records[key]), step)
+
+                print(f'[Meta Valid]({step+1}/{self.total_steps})')
+                for key in ['Accuracy', 'Loss']:
+                    print(f'  Task {key}: {np.mean(valid_records[key]):.4f}')
+
+                cur_eval_loss = np.mean(valid_records['Loss'])
+                cur_eval_acc = np.mean(valid_records['Accuracy'])
+                if cur_eval_acc > best_eval_acc:
+                    best_eval_acc = cur_eval_acc 
+                    torch.save(model.state_dict(), str(self.ckpt_path / f'{step}-{cur_eval_acc:.4f}-{cur_eval_loss:.4f}.ckpt'))
+
+    def meta_test(self, meta_test1, meta_test2, meta_test3):
+        pass
