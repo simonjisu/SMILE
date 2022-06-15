fb7d2bce1e4a999f6025b4b4f8975e95f9ec9cd6
simonjisu <simonjisu@gmail.com>
2022-06-15 19:27:44 +0900

    enable nohup training

--- run_train.sh
+++ run_train.sh
@@ -1,1 +1,8 @@
-python main.py --exp $1
+if [ $# -eq 0 ]; then 
+    python main.py --exp $1
+elif [ "$1" = "nohup" ]; then
+    nohup python -u main.py --exp $1 > $1.log &
+else
+    echo "If you want to run train with nohup, please insert 'nohup' as arugment"
+fi
+
