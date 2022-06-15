45cdff583a1f1e47f0df421c31ac4941903f9fda
simonjisu <simonjisu@gmail.com>
2022-06-15 12:49:00 +0900

    update zip related things: ignore push & extraction sh file

--- zip_results.sh
+++ zip_results.sh
@@ -1,1 +1,7 @@
-zip -r results.zip ./logging/* -x ./logging/*/events.out.tfevents.*
+if [ $# -eq 0 ]; then 
+    zip -r results.zip ./logging/*
+elif [ "$1" = "model" ]; then
+    zip -r results.zip ./logging/* -x ./logging/*/events.out.tfevents.*
+else
+    echo "If you want to exclude tensorboard object insert 'model'"
+fi
